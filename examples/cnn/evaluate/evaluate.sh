c52fa9d2fb7ad02aacb0c0bc1ee0c6c1e2cf953e
KEHANG <kehanghan@gmail.com>
2018-05-09 14:24:39 -0400

    use the right environment name

--- examples/cnn/evaluate/evaluate.sh
+++ examples/cnn/evaluate/evaluate.sh
@@ -2,7 +2,7 @@
 
 CNN_MODEL='test_model'
 DATA_FILE='test_datasets.txt'
-source activate rmg_env
+source activate dde_env
 export KERAS_BACKEND=theano
 python ../../../scripts/evaluate_cnn.py -d ${DATA_FILE} -m ${CNN_MODEL}
 source deactivate
--- examples/cnn/train/train.sh
+++ examples/cnn/train/train.sh
@@ -6,7 +6,7 @@ TRAIN_MODE='full_train'
 BATCH_SIZE=1
 NB_EPOCH=1
 PATIENCE=1
-source activate rmg_env
+source activate dde_env
 export KERAS_BACKEND=theano
 python ../../../scripts/train_cnn.py -i $INPUT -d ${DATA_FILE} -t ${TRAIN_MODE} -bs ${BATCH_SIZE} -ep ${NB_EPOCH} -pc ${PATIENCE}
 source deactivate
