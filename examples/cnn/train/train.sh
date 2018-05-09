ea155b966de852ca89825f05f706dda2e73170e8
KEHANG <kehanghan@gmail.com>
2018-05-09 13:34:45 -0400

    add cnn training script and example

---
+++ examples/cnn/train/train.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INPUT='predictor_input.py'
+DATA_FILE='datasets.txt'
+TRAIN_MODE='full_train'
+BATCH_SIZE=1
+NB_EPOCH=1
+PATIENCE=1
+source activate rmg_env
+export KERAS_BACKEND=theano
+python ../../../scripts/train_cnn.py -i $INPUT -d ${DATA_FILE} -t ${TRAIN_MODE} -bs ${BATCH_SIZE} -ep ${NB_EPOCH} -pc ${PATIENCE}
+source deactivate
